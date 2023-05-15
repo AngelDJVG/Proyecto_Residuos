c51aaa8ffa6a8eedfcabad8b9cf14e85a1e47a41
PabolOne <luis.ayon233145@potros.itson.edu.mx>
2023-05-14 17:27:11 -0700

    Conexion

---
+++ CapaConexion/src/main/java/org/itson/capaconexion/Conexion.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.capaconexion;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+/**
+ *
+ * @author lv1013
+ */
+public class Conexion {
+    public  MongoDatabase base()
+    {
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoClientSettings settings = MongoClientSettings.builder()
+        .codecRegistry(pojoCodecRegistry)
+        .build();
+        com.mongodb.client.MongoClient conexion = MongoClients.create(settings);
+         MongoDatabase database = conexion.getDatabase("RestaurantesBD_10am");
+         return database;
+    }
+}
