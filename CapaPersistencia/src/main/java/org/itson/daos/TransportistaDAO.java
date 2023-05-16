546189d9e95c20d61b10d51bff6c3b0e57ee4ff6
MarioZDR <125427714+MarioZDR@users.noreply.github.com>
2023-05-15 17:06:00 -0700

    Actualización del TransportistaDAO

--- CapaPersistencia/src/main/java/org/itson/daos/TransportistaDAO.java
+++ CapaPersistencia/src/main/java/org/itson/daos/TransportistaDAO.java
@@ -6,71 +6,86 @@ package org.itson.daos;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import org.bson.types.ObjectId;
 import org.itson.dominio.Transportista;
+import org.itson.registros.Interface.IConexionBD;
 
 /**
- * 
- * @author Equipo 3 
- * Luis Pablo Ayón Figueroa
- * Mario Díaz Padilla
- * Gabriel Alberto Mancinas Cota
- * Ángel de Jesús Valenzuela García
+ *
+ * @author Equipo 3 Luis Pablo Ayón Figueroa Mario Díaz Padilla Gabriel Alberto
+ * Mancinas Cota Ángel de Jesús Valenzuela García
  */
-public class TransportistaDAO extends DAOGeneral<Transportista>{
+public class TransportistaDAO extends DAOGeneral<Transportista> {
+
     private final String NOMBRE_COLECCION = "transportistas";
     private final MongoDatabase BASE_DATOS;
 
-    public TransportistaDAO(ConexionBD CONEXION) {
+    public TransportistaDAO(IConexionBD CONEXION) {
         this.BASE_DATOS = CONEXION.getBaseDatos();
     }
 
     @Override
     public Transportista agregar(Transportista transportista) {
-           MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION,Transportista.class);
-           collection.insertOne(transportista);
-           return transportista;
+        MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION, Transportista.class);
+        collection.insertOne(transportista);
+        return transportista;
     }
-    
+
     public void agregarPredeterminados() {
-           MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION,Transportista.class);
-           Transportista transportista = new Transportista("Mario Diaz", "mario.diaz233259@potros.itson.edu.mx");
-           Transportista transportista1 = new Transportista("Julio Chon", "julio.chon233242@potros.itson.edu.mx");
-           Transportista transportista2 = new Transportista("Adrian Macias", "adrian.macias@itson.edu.mx");
-           Transportista transportista3 = new Transportista("Angel Valenzuela", "angel.valenzuela233109@potros.itson.edu.mx");
-           Transportista transportista4 = new Transportista("Gabriel Mancinas", "gabriel.mancinas233410@potros.itson.edu.mx");
-           Transportista transportista5 = new Transportista("Gibran Duran", "christian.duran@potros.itson.edu.mx ");
-           List<Transportista> transportistas = new ArrayList<>();
-           transportistas.add(transportista);
-           transportistas.add(transportista1);
-           transportistas.add(transportista2);
-           transportistas.add(transportista3);
-           transportistas.add(transportista4);
-           transportistas.add(transportista5);
-           collection.insertMany(transportistas);
+        MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION, Transportista.class);
+        Transportista transportista = new Transportista("Mario Diaz", "mario.diaz233259@potros.itson.edu.mx");
+        Transportista transportista1 = new Transportista("Julio Chon", "julio.chon233242@potros.itson.edu.mx");
+        Transportista transportista2 = new Transportista("Adrian Macias", "adrian.macias@itson.edu.mx");
+        Transportista transportista3 = new Transportista("Angel Valenzuela", "angel.valenzuela233109@potros.itson.edu.mx");
+        Transportista transportista4 = new Transportista("Gabriel Mancinas", "gabriel.mancinas233410@potros.itson.edu.mx");
+        Transportista transportista5 = new Transportista("Gibran Duran", "christian.duran@potros.itson.edu.mx ");
+        List<Transportista> transportistas = new ArrayList<>();
+        transportistas.add(transportista);
+        transportistas.add(transportista1);
+        transportistas.add(transportista2);
+        transportistas.add(transportista3);
+        transportistas.add(transportista4);
+        transportistas.add(transportista5);
+        collection.insertMany(transportistas);
     }
 
     @Override
     public Transportista actualizar(Transportista entidad) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        MongoCollection<Transportista> coleccion = BASE_DATOS.getCollection(NOMBRE_COLECCION, Transportista.class);
+        coleccion.replaceOne(eq("_id", entidad.getId()), entidad);
+        return entidad;
     }
 
     @Override
     public Transportista consultar(ObjectId id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION, Transportista.class);
+        List<Transportista> transportistas = new LinkedList<>();
+        collection.find(eq("_id", id)).into(transportistas);
+        return transportistas.isEmpty() ? null : transportistas.get(0);
     }
-
 
     @Override
     public List<Transportista> consultarTodos() {
-        MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION,Transportista.class);
+        MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION, Transportista.class);
         List<Transportista> transportistas = new LinkedList<>();
         collection.find().into(transportistas);
-        
+
         return transportistas;
+    }
+
+    public List<String> consultarCorreosTransportistas() {
+        List<String> correos = new LinkedList<>();
+        MongoCollection<Transportista> collection = BASE_DATOS.getCollection(NOMBRE_COLECCION, Transportista.class);
+        List<Transportista> transportistas = new LinkedList<>();
+        collection.find().into(transportistas);
+        for (Transportista transportista : transportistas) {
+            correos.add(transportista.getEmail());
+        }
+        return correos;
     }
 
 }
